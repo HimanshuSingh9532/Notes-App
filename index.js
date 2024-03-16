5c191d09fc93d1d10a221cb5e43626ce033ae646
Himanshu Singh <shimanshusingh9532@gmail.com>
2024-03-16 22:48:57 +0530

    js update

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 //alert("hi");
+let notesContainer = document.querySelector(".notes-container");
+let createBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
+
+createBtn.addEventListener("click", () => {
+   let inputBox = document.createElement("p");
+   let img = document.createElement("img");
+   inputBox.className = "inputbox";
+   inputBox.setAttribute("contenteditable","true" );
+   img.src = "./notes-app-img/images/delete.png";
+   notesContainer.appendChild(inputBox).appendChild(img);
+
+});
